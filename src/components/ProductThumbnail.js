00558a247e8e7d1fc1467f71dd16530ec1d3f264
Sarah <sarah@sekai.co.uk>
2019-09-01 16:46:43 +0100

    Add default product thumbnail

--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -5,7 +5,10 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
+const noImageFound = require("../images/no-image-found.jpg");
+
 const ProductThumbnail = ({ product }) => {
+    console.log(product);
     return (
         <div key={product.id} className="product-box p-4 rounded">
             <Link
@@ -13,12 +16,17 @@ const ProductThumbnail = ({ product }) => {
                 style={{ textDecoration: "none" }}
             >
                 <div>
-                    {product.localFiles && (
-                        <Img
-                            fluid={product.localFiles[0].childImageSharp.fluid}
-                            alt={product.name}
-                        />
-                    )}
+                    <div className="text-center">
+                        {product.localFiles && (
+                            <Img
+                                fluid={
+                                    product.localFiles[0].childImageSharp.fluid
+                                }
+                                alt={product.name}
+                            />
+                        )}
+                        {!product.localFiles && <img src={noImageFound} />}
+                    </div>
                     <div className="mt-3 d-flex justify-content-between">
                         <div className="font-weight-bold">{product.name}</div>
                         <div>£{product.skus[0].price / 100}</div>
