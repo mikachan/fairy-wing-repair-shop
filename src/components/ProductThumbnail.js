2675002503857341e5afb56089aa6a9be732e141
Sarah <sarah@sekai.co.uk>
2019-09-01 13:19:14 +0100

    Fix unclosed JSX tags

--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -1,37 +1,41 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "gatsby";
 import Img from "gatsby-image";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
 
 const ProductThumbnail = ({ product }) => {
-  return (
-    <div key={product.id} style={{ breakInside: 'avoid' }}>
-      <Link to={`/buy/${product.slug}`} style={{ textDecoration: 'none' }}>
-        <div
-          style={{
-            maxWidth: 250,
-            flex: '1 1 auto',
-            margin: '0 2rem 3rem'
-          }}
-        >
-          {product.localFiles && (
-            <Img
-              fluid={product.localFiles[0].childImageSharp.fluid}
-              alt={product.name}
+    return (
+        <div key={product.id} style={{ breakInside: "avoid" }}>
+            <Link
+                to={`/buy/${product.slug}`}
+                style={{ textDecoration: "none" }}
             />
-          )}
-          <div
-            style={{
-              fontWeight: 'bold',
-              textAlign: 'center',
-              marginTop: '0.5rem'
-            }}
-          >
-            {product.name}
-          </div>
-          <div style={{ textAlign: 'center' }}>
-            ${product.skus[0].price / 100}
-          </div>
+            <div
+                style={{
+                    maxWidth: 250,
+                    flex: "1 1 auto",
+                    margin: "0 2rem 3rem"
+                }}
+            >
+                {product.localFiles && (
+                    <Img
+                        fluid={product.localFiles[0].childImageSharp.fluid}
+                        alt={product.name}
+                    />
+                )}
+                <div
+                    style={{
+                        fontWeight: "bold",
+                        textAlign: "center",
+                        marginTop: "0.5rem"
+                    }}
+                >
+                    {product.name}
+                </div>
+                <div style={{ textAlign: "center" }}>
+                    ${product.skus[0].price / 100}
+                </div>
+            </div>
         </div>
     );
 };
