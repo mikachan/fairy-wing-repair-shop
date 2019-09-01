5ece0ed76819d93a78bb75d4066df6bb53bb737d
Sarah <sarah@sekai.co.uk>
2019-09-01 13:44:55 +0100

    Add link to product thumbnail

--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -9,33 +9,34 @@ const ProductThumbnail = ({ product }) => {
             <Link
                 to={`/buy/${product.slug}`}
                 style={{ textDecoration: "none" }}
-            />
-            <div
-                style={{
-                    maxWidth: 250,
-                    flex: "1 1 auto",
-                    margin: "0 2rem 3rem"
-                }}
             >
-                {product.localFiles && (
-                    <Img
-                        fluid={product.localFiles[0].childImageSharp.fluid}
-                        alt={product.name}
-                    />
-                )}
                 <div
                     style={{
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        marginTop: "0.5rem"
+                        maxWidth: 250,
+                        flex: "1 1 auto",
+                        margin: "0 2rem 3rem"
                     }}
                 >
-                    {product.name}
+                    {product.localFiles && (
+                        <Img
+                            fluid={product.localFiles[0].childImageSharp.fluid}
+                            alt={product.name}
+                        />
+                    )}
+                    <div
+                        style={{
+                            fontWeight: "bold",
+                            textAlign: "center",
+                            marginTop: "0.5rem"
+                        }}
+                    >
+                        {product.name}
+                    </div>
+                    <div style={{ textAlign: "center" }}>
+                        £{product.skus[0].price / 100}
+                    </div>
                 </div>
-                <div style={{ textAlign: "center" }}>
-                    ${product.skus[0].price / 100}
-                </div>
-            </div>
+            </Link>
         </div>
     );
 };
